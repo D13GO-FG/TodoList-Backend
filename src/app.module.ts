112cbadb6271a1731aa58d9c2c56f107feda21f0
D13GO-FG <ldiego.flores98@gmail.com>
2022-06-19 21:01:19 -0700

    Created API to signin and signup, also to create new task, change status and get tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './model/user.schema';
+import { UserController } from './controller/user.controller';
+import { UserService } from './service/user.service';
+import { isAuthenticated } from './app.middleware';
+import { JwtModule } from '@nestjs/jwt';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { secret } from './utils/constants';
+import { join } from 'path/posix';
+import { TaskController } from './controller/task.controller';
+import { Task, TaskSchema } from './model/task.schema';
+import { TaskService } from './service/task.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/todolist'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'public'),
+    // }),
+  ],
+  controllers: [AppController, UserController, TaskController],
+  providers: [AppService, UserService, TaskService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(isAuthenticated)
+      .exclude({ path: 'api/v1/task/:id', method: RequestMethod.GET })
+      .forRoutes(TaskController);
+  }
+}
---
+++ src/service/task.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task, TaskDocument } from '../model/task.schema';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class TaskService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>) {}
+
+  async addTask(task: Task): Promise<Task> {
+    const reqBody = {
+      description: task.description,
+      status: task.status,
+      createdBy: task.createdBy,
+    };
+    const newUser = await new this.taskModel(reqBody);
+    return newUser.save();
+  }
+
+  async updateTask(id: string, task: Task): Promise<Task> {
+    return await this.taskModel
+      .findByIdAndUpdate(id, task, { new: true })
+      .exec();
+  }
+
+  async getListId(_id: any): Promise<Task[]> {
+    return await this.taskModel.find({ createdBy: _id }).exec();
+  }
+
+  async getList(): Promise<Task[]> {
+    return await this.taskModel.find().populate('createdBy').exec();
+  }
+}
